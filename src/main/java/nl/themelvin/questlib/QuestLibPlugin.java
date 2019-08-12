458b40a989931b9d066596f546c0ed89361d0e4f
Melvin Snijders <mail@melvinsnijders.nl>
2019-08-12 22:25:48 +0200

    :fire: Moved example to other repository

--- src/main/java/nl/themelvin/questlib/QuestLibPlugin.java
+++ src/main/java/nl/themelvin/questlib/QuestLibPlugin.java
@@ -1,14 +1,6 @@
 package nl.themelvin.questlib;
 
-import nl.themelvin.questlib.example.BreakQuest;
-import nl.themelvin.questlib.example.CobblestoneObjective;
-import nl.themelvin.questlib.example.DirtObjective;
-import nl.themelvin.questlib.example.JoinListener;
-import nl.themelvin.questlib.storage.types.YAML;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.io.File;
 
 public class QuestLibPlugin extends JavaPlugin {
 
@@ -16,14 +8,6 @@ public class QuestLibPlugin extends JavaPlugin {
     public void onEnable() {
 
         this.saveDefaultConfig();
-
-        QuestLib.getInstance().registerQuest("break", BreakQuest.class);
-        QuestLib.getInstance().registerObjective("cobblestone", CobblestoneObjective.class);
-        QuestLib.getInstance().registerObjective("dirt", DirtObjective.class);
-
-        QuestLib.getInstance().init(this, new YAML(new File(this.getDataFolder() + "/data", "data.yml")));
-
-        Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
 
     }
 
